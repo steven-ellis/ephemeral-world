a4ffa1bc81e9c9f0f573875af85d3a7a29de813b
Steven Ellis <steven.ellis@gmail.com>
2022-01-13 17:35:42 +1300

    Initial scripted alternative to docker file for buildah, refs #350 #351

---
+++ buildah/ubi-minimal-httpd.sh
@@ -0,0 +1,21 @@
+# -l/usr/bin/env bash
+
+set -x
+
+ctr1=$(buildah from "${1:-registry.access.redhat.com/ubi8/ubi-minimal:latest}")
+
+## Get all updates and install our minimal httpd server
+buildah run "$ctr1" -- microdnf update -y
+buildah run "$ctr1" -- microdnf install -y httpd
+
+## Include some buildtime annotations
+buildah config --annotation "com.example.build.host=$(uname -n)" "$ctr1"
+
+## Run our server and expose the port
+buildah config --cmd "/usr/sbin/httpd -DFOREGROUND" "$ctr1"
+buildah config --port 80 "$ctr1"
+
+## Commit this container to an image name
+buildah commit "$ctr1" "${2:-ubi-minimal-scripted-httpd}"
+
+
